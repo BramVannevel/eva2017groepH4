a9327f7d4d662cc6f0d51e4d4892d243f3c20658
Sander Brugge <sander.brugge@moovly.com>
2017-07-21 21:52:10 +0200

    facebook login

---
+++ Android/EVA/app/src/main/java/com/projecten3/eva/MainActivity.java
@@ -0,0 +1,46 @@
+package com.projecten3.eva;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+
+        loginButton.setReadPermissions("email");
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.i(TAG, "onsucces");
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.i(TAG, "oncancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.i(TAG, "onerror");
+            }
+        });
+    }
+}
