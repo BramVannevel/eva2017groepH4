c15518033ad672c19af63b696f3f26aed4a3bb12
Sander Brugge <sander.brugge@moovly.com>
2017-07-31 19:37:21 +0200

    register page in same UI as login

--- Android/EVA/app/src/main/java/com/projecten3/eva/MainActivity.java
+++ Android/EVA/app/src/main/java/com/projecten3/eva/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
+import android.widget.Button;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -12,18 +12,32 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 public class MainActivity extends AppCompatActivity {
     private final String TAG = getClass().getSimpleName();
+    private Unbinder unbinder;
 
+    @BindView(R.id.fb_btn_login)
+    LoginButton facebookLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LoginButton loginButton = (LoginButton) findViewById(R.id.fb_btn_login);
-        CallbackManager callbackManager = CallbackManager.Factory.create();
+        unbinder = ButterKnife.bind(this);
+        facebookLoginCallback();
+    }
 
-        loginButton.setReadPermissions("email");
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+    /**
+     * basis implementatie van de callback van de facebook login activity zoals gezien op dev.fb.com
+     */
+    private void facebookLoginCallback(){
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+        facebookLogin.setReadPermissions("email");
+        facebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.i(TAG, "onsucces");
@@ -39,5 +53,23 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "onerror");
             }
         });
+    }
+
+    /**
+     * onclick van Butterknife verwacht een public methode
+     */
+    @OnClick(R.id.btn_sign_up)
+    public void onSignUpClickListener(){
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    /**
+     * unbind butterknife in the ondestroy to avoid memory leaks
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(unbinder != null)
+            unbinder.unbind();
     }
 }
