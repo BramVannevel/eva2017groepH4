ef8a9f32d6c4f520c0b44fdd401a02d456145207
Sander Brugge <sander.brugge@moovly.com>
2017-07-31 19:03:43 +0200

    login page with better UI

--- Android/EVA/app/src/main/java/com/projecten3/eva/MainActivity.java
+++ Android/EVA/app/src/main/java/com/projecten3/eva/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -18,27 +19,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.fb_btn_login);
         CallbackManager callbackManager = CallbackManager.Factory.create();
 
         loginButton.setReadPermissions("email");
-        // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                // App code
                 Log.i(TAG, "onsucces");
             }
 
             @Override
             public void onCancel() {
-                // App code
                 Log.i(TAG, "oncancel");
             }
 
             @Override
             public void onError(FacebookException exception) {
-                // App code
                 Log.i(TAG, "onerror");
             }
         });
