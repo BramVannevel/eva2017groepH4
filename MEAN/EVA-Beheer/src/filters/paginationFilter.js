125f58da2eaa124d2007533506ada00efc2e4945
Dennis Noens <dennis.noens@telenet.be>
2017-08-03 17:34:19 +0200

    Bugfix for the paginationFilter

--- MEAN/EVA-Beheer/src/filters/paginationFilter.js
+++ MEAN/EVA-Beheer/src/filters/paginationFilter.js
@@ -5,6 +5,7 @@ const paginationFilter = angular.module('app.paginationFilter', [])
 //To be called in the ng-repeat statement in the html
 .filter('paginationFilter', ['$filter', function($filter) {
     return function(data, start) {
+      if (!data || !data.length) { return; }
       return data.slice(start);
     };
 }]);
