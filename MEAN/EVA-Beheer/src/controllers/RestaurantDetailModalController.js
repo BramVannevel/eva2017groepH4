95ad5e09c85a759c7b0a9e8cb4df159a02dd3572
Dennis Noens <dennis.noens@telenet.be>
2017-08-06 13:54:23 +0200

    When nothing is changed to the data of a restaurant saving isn't possible anymore.

--- MEAN/EVA-Beheer/src/controllers/RestaurantDetailModalController.js
+++ MEAN/EVA-Beheer/src/controllers/RestaurantDetailModalController.js
@@ -12,26 +12,29 @@ const restaurantDetailModalController = angular.module('app.restaurantDetailModa
   };
 
   $scope.ok = function(updatedRestaurant) {
+  if(updatedRestaurant !== undefined) {
   //Waar niks veranderd is, gebruik de oorspronkelijke waarde
-  updatedRestaurant._id = $scope.restaurant._id;
-  if(!updatedRestaurant.naam)
-    updatedRestaurant.naam = $scope.restaurant.naam;
-   if (!updatedRestaurant.telefoon)
-    updatedRestaurant.telefoon = $scope.restaurant.telefoon;
-  if (!updatedRestaurant.adres.straat)
-    updatedRestaurant.adres.straat = $scope.restaurant.adres.straat;
-  if (!updatedRestaurant.adres.huisnummer)
-    updatedRestaurant.adres.huisnummer = $scope.restaurant.adres.huisnummer;
-  if (!updatedRestaurant.adres.stad)
-    updatedRestaurant.adres.stad = $scope.restaurant.adres.stad;
-  if (!updatedRestaurant.adres.postcode)
-    updatedRestaurant.adres.postcode = $scope.restaurant.adres.postcode;
+      updatedRestaurant._id = $scope.restaurant._id;
+      if(!updatedRestaurant.naam)
+        updatedRestaurant.naam = $scope.restaurant.naam;
+       if (!updatedRestaurant.telefoon)
+        updatedRestaurant.telefoon = $scope.restaurant.telefoon;
+      if (!updatedRestaurant.adres.straat)
+        updatedRestaurant.adres.straat = $scope.restaurant.adres.straat;
+      if (!updatedRestaurant.adres.huisnummer)
+        updatedRestaurant.adres.huisnummer = $scope.restaurant.adres.huisnummer;
+      if (!updatedRestaurant.adres.stad)
+        updatedRestaurant.adres.stad = $scope.restaurant.adres.stad;
+      if (!updatedRestaurant.adres.postcode)
+        updatedRestaurant.adres.postcode = $scope.restaurant.adres.postcode;
 
+      console.log("Gewijzigd huisnummer: " + updatedRestaurant.adres.huisnummer);
+      console.log("Naam: " + updatedRestaurant.naam);
 
-  console.log("Gewijzigd huisnummer: " + updatedRestaurant.adres.huisnummer);
-  console.log("Naam: " + updatedRestaurant.naam);
-
-    $uibModalInstance.close(updatedRestaurant);
+      $uibModalInstance.close(updatedRestaurant);
+    } else {
+      return;
+    }
   };
 });
 
