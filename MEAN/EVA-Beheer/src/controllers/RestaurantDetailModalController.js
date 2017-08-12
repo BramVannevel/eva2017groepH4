17529a3c1811699d8c5e233409bb3e27dadd8ccf
Dennis Noens <dennis.noens@telenet.be>
2017-08-12 13:45:19 +0200

    Bugfix: Adress is no longer undefined when when it isn't changed in the restaurant detail modal.

--- MEAN/EVA-Beheer/src/controllers/RestaurantDetailModalController.js
+++ MEAN/EVA-Beheer/src/controllers/RestaurantDetailModalController.js
@@ -19,14 +19,18 @@ const restaurantDetailModalController = angular.module('app.restaurantDetailModa
         updatedRestaurant.naam = $scope.restaurant.naam;
        if (!updatedRestaurant.telefoon)
         updatedRestaurant.telefoon = $scope.restaurant.telefoon;
-      if (!updatedRestaurant.adres.straat)
-        updatedRestaurant.adres.straat = $scope.restaurant.adres.straat;
-      if (!updatedRestaurant.adres.huisnummer)
-        updatedRestaurant.adres.huisnummer = $scope.restaurant.adres.huisnummer;
-      if (!updatedRestaurant.adres.stad)
-        updatedRestaurant.adres.stad = $scope.restaurant.adres.stad;
-      if (!updatedRestaurant.adres.postcode)
-        updatedRestaurant.adres.postcode = $scope.restaurant.adres.postcode;
+        if(!updatedRestaurant.adres) {
+          updatedRestaurant.adres = $scope.restaurant.adres;
+        } else {
+            if (!updatedRestaurant.adres.straat)
+            updatedRestaurant.adres.straat = $scope.restaurant.adres.straat;
+            if (!updatedRestaurant.adres.huisnummer)
+              updatedRestaurant.adres.huisnummer = $scope.restaurant.adres.huisnummer;
+            if (!updatedRestaurant.adres.stad)
+              updatedRestaurant.adres.stad = $scope.restaurant.adres.stad;
+            if (!updatedRestaurant.adres.postcode)
+              updatedRestaurant.adres.postcode = $scope.restaurant.adres.postcode;
+      }
 
       console.log("Gewijzigd huisnummer: " + updatedRestaurant.adres.huisnummer);
       console.log("Naam: " + updatedRestaurant.naam);
