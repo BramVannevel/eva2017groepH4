745e00909a06724f7e3170068830fe44fb3c39cc
Dennis Noens <dennis.noens@telenet.be>
2017-08-13 11:17:27 +0200

    Base for vegagram endpoints.

---
+++ MEAN/EVA-Beheer/src/server/vegagram/routes.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Vegagram = require('../db/models/Vegagram');
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var config = require('../db/db');
+var jwt = require('jwt-simple');
+
+
+
+module.exports = router;
